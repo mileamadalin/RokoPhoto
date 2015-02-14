60083efad87e660be0cd626a6e429ebe41d5b0c2
Hardeep Asrani <hardeepasrani@gmail.com>
2015-02-14 09:15:07 +0530

    Minor fix.

--- functions.php
+++ functions.php
@@ -168,7 +168,7 @@ function rokophotolite_pagination() {
 	/**	Link to last page, plus ellipses if necessary */
 	if ( ! in_array( $max, $links ) ) {
 		if ( ! in_array( $max - 1, $links ) )
-			echo '<li>…</li>' . "\n";
+			echo '<li><span>…</span></li>' . "\n";
 
 		$class = $paged == $max ? ' class="active"' : '';
 		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
