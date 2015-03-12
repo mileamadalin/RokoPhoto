ef47155eac49eccb5895dc21cf00120da7ffee27
Hardeep Asrani <me@hardeepasrani.com>
2015-03-12 06:30:21 +0530

    Portfolio Now Opens In A Modal
    Portfolio files now opens in a modal, including the default items. :)

--- sections/portfolio.php
+++ sections/portfolio.php
@@ -32,7 +32,7 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 				<figcaption> <!-- Caption -->
 					<h2><?php the_title(); ?></h2>
 					<?php the_excerpt(); ?>
-					<a href="<?php the_permalink(); ?>" class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+					<a href="#portfolioModa<?php the_ID(); ?>" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 				</figcaption> <!-- End caption -->   
 			</figure> <!-- End portfolio -->
 	
@@ -45,7 +45,7 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 			<figcaption> <!-- Caption -->
 				<h2><?php _e( 'Item 1', 'rokophoto' ); ?></h2>
 				<p><?php _e( 'Hi, my name is description.', 'rokophoto' ); ?></p>
-				<a class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+				<a href="#portfolioModal000" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 			</figcaption> <!-- End caption -->   
 		</figure> <!-- End portfolio -->
 
@@ -54,7 +54,7 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 			<figcaption> <!-- Caption -->
 				<h2><?php _e( 'Item 2', 'rokophoto' ); ?></h2>
 				<p><?php _e( 'Hi, my name is description.', 'rokophoto' ); ?></p>
-				<a class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+				<a href="#portfolioModal000" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 			</figcaption> <!-- End caption -->   
 		</figure> <!-- End portfolio -->
 
@@ -63,7 +63,7 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 			<figcaption> <!-- Caption -->
 				<h2><?php _e( 'Item 3', 'rokophoto' ); ?></h2>
 				<p><?php _e( 'Hi, my name is description.', 'rokophoto' ); ?></p>
-				<a class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+				<a href="#portfolioModal000" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 			</figcaption> <!-- End caption -->   
 		</figure> <!-- End portfolio -->
 	
@@ -72,7 +72,7 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 			<figcaption> <!-- Caption -->
 				<h2><?php _e( 'Item 4', 'rokophoto' ); ?></h2>
 				<p><?php _e( 'Hi, my name is description.', 'rokophoto' ); ?></p>
-				<a class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+				<a href="#portfolioModal000" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 			</figcaption> <!-- End caption -->   
 		</figure> <!-- End portfolio -->
 
@@ -81,7 +81,7 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 			<figcaption> <!-- Caption -->
 				<h2><?php _e( 'Item 5', 'rokophoto' ); ?></h2>
 				<p><?php _e( 'Hi, my name is description.', 'rokophoto' ); ?></p>
-				<a class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+				<a href="#portfolioModal000" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 			</figcaption> <!-- End caption -->   
 		</figure> <!-- End portfolio -->
 
@@ -90,9 +90,65 @@ $rokophoto_portfolio_count = get_theme_mod('rokophoto_portfolio_count', 9);
 			<figcaption> <!-- Caption -->
 				<h2><?php _e( 'Item 6', 'rokophoto' ); ?></h2>
 				<p><?php _e( 'Hi, my name is description.', 'rokophoto' ); ?></p>
-				<a class="portfolio-link" ><?php _e( 'View More', 'rokophoto' ); ?></a>
+				<a href="#portfolioModal000" class="portfolio-link" data-toggle="modal"><?php _e( 'View More', 'rokophoto' ); ?></a>
 			</figcaption> <!-- End caption -->   
 		</figure> <!-- End portfolio -->
+		
+		<div class="portfolio-modal modal fade" id="portfolioModal000" tabindex="-1" role="dialog" aria-hidden="true">
+			<div class="modal-content">
+				<div class="close-modal" data-dismiss="modal">
+					<div class="lr">
+						<div class="rl">
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 col-lg-offset-2">
+							<div class="modal-body">
+								<h2><?php _e( 'Portfolio Title', 'rokophoto' ); ?></h2>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/img/550x400.gif'; ?>" />
+								<?php _e( '<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+								<p><strong>Want these theme for your portfolio?</strong> RokoPhoto is a bootstrap based awesome photography portfolio template. Creatively designed to present your artwork beautifully and clean. Catch more visitors and potential clients by using Solstice Photography/Portfolio Template to showcase your photography and crafts. This is mobile responsive template, very easy to use and well-documented codes.</p>', 'rokophoto' ); ?>
+								<button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i><?php _e( ' Close', 'rokophoto' ); ?></button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	
 	<?php endif; wp_reset_postdata(); ?>
+	
+	<?php $secondloop = new WP_Query( array( 'post_type' => 'portfolio', 'posts_per_page' => $rokophoto_portfolio_count ) ); ?>
+
+	<?php if ( $secondloop->have_posts() ): ?>
+	
+		<?php while ( $secondloop->have_posts() ) : $secondloop->the_post(); ?>
+			<div class="portfolio-modal modal fade" id="portfolioModa<?php the_ID(); ?>" tabindex="-1" role="dialog" aria-hidden="true">
+				<div class="modal-content">
+					<div class="close-modal" data-dismiss="modal">
+						<div class="lr">
+							<div class="rl">
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8 col-lg-offset-2">
+								<div class="modal-body">
+									<h2><?php the_title(); ?></h2>
+									<?php the_post_thumbnail( 'full', array( 'class' => "img-responsive")); ?>
+									<?php the_content(); ?>
+									<button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i><?php _e( ' Close', 'rokophoto' ); ?></button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
+
+	<?php endif; wp_reset_postdata(); ?>
+
     </section> <!-- End Portfolio Section -->
